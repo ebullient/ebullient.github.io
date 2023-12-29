e5aca4727a691efc34872ce6afbf48997e119f69
GitHub <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 14:48:48 +0000

    🆙 Auto-update submodule references

--- .github/update-pages.js
+++ .github/update-pages.js
@@ -33,14 +33,15 @@ layout: single
 function addFrontMatter(file) {
   // Regular expression to match .json, .yaml, .css, and .txt links
   const codeFiles = /\(([^)]*?\.(json|yaml|css|txt))\)/g;
-  const htmlLinks = /(href="[^"]*)\.md(#\w+?")/g;
+  const htmlLinks = /(href="[^"]*\.)md((#[^"]*)?")/g;
   const content = fs.readFileSync(file, 'utf8').replace(codeFiles, (match, group1) => {
     if (group1.startsWith('http')) {
       return match;
     }
     return `(${group1.replace('index.txt', 'index-template')}.md)`;
   }).replace(htmlLinks, (match, group1, group2) => {
-    return `${group1}${group2.replace('.md', 'html')}`
+    console.log(match, group1, group2);
+    return `${group1}html${group2}`;
   });
   const fileName = path.basename(file);
   let newFrontMatter = singleFrontMatter;
